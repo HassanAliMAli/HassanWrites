7eff59d301df261833b9eca7a2ebcafbe863a7c7
AlM1GhTy <hassanali205031@gmail.com>
2025-11-02 23:53:09 +0500

    chore: reset and reconfigure project

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,30 +8,30 @@ export default defineConfig({
   site: 'https://HassanAliMAli.github.io',
   base: '/HassanWrites',
   integrations: [
-    // sitemap(),
-    // AstroPWA({
-    //   registerType: 'autoUpdate',
-    //   workbox: {
-    //     globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-    //   },
-    //   manifest: {
-    //     name: 'My Awesome Blog',
-    //     short_name: 'MyBlog',
-    //     description: 'My Awesome Blog description',
-    //     theme_color: '#ffffff',
-    //     icons: [
-    //       {
-    //         src: 'pwa-192x192.png',
-    //         sizes: '192x192',
-    //         type: 'image/png'
-    //       },
-    //       {
-    //         src: 'pwa-512x512.png',
-    //         sizes: '512x512',
-    //         type: 'image/png'
-    //       }
-    //     ]
-    //   }
-    // })
+    sitemap(),
+    AstroPWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      manifest: {
+        name: 'My Awesome Blog',
+        short_name: 'MyBlog',
+        description: 'My Awesome Blog description',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ]
 });
