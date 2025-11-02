2ff5e7565be0333b9cfce6e84d24e8d5b649e4a6
AlM1GhTy <hassanali205031@gmail.com>
2025-11-02 20:17:29 +0500

    fix: update site and base in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://your-domain.com',
+  site: 'https://HassanAliMAli.github.io',
+  base: '/HassanWrites',
   integrations: [
     sitemap(),
     AstroPWA({
